7212d401e6847f3e7c3a486d41cb7b02c30dd6e6
Anne Cori <a.cori@imperial.ac.uk>
2017-05-25 11:19:12 +0100

    now getting results of estimated incidence for ProMED data - getting very different results from Healthmap data

--- workflow.R
+++ workflow.R
@@ -1,5 +1,9 @@
 rm(list=ls())
 
+library(devtools)
+install_github("annecori/mRIIDSprocessData")
+library(mRIIDSprocessData)
+
 ###############################
 ### arguments that user may want to change
 ###############################
@@ -27,17 +31,18 @@ dat <- read.csv(filename, stringsAsFactors = FALSE)
 ####################################
 
 incid <- get_incid_DS1(dat, 
-                                   spec, 
-                                   disease, 
-                                   case_type, 
-                                   location, 
-                                   merge_rule = "median")
+                       spec, 
+                       disease, 
+                       case_type, 
+                       location, 
+                       merge_rule = "median")
 
 
 
 ### visualise incidence
-par(mfrow=c(1,1))
-plot(incid$dates, incid$incid, type="l", xlab="Time", ylab="Incidence")
+par(mfrow=c(2,2))
+plot(incid$dates, incid$cum_incid, type="l", xlab="Time", ylab="Cumulative Incidence", xlim= c(as.Date("2014-01-01"), as.Date("2016-12-12")))
+plot(incid$dates, incid$incid, type="l", xlab="Time", ylab="Incidence", xlim= c(as.Date("2014-01-01"), as.Date("2016-12-12")))
 ### To do: add our estimate of daily incidence
 
 ##############################################################################################################################
@@ -62,22 +67,40 @@ process_names_promed_dat <- function(dat)
   return(dat)
 }
 
+process_issue_dates_promed_dat <- function(dat)
+{
+  tmp <- unlist(strsplit(dat$Issue.Date, " "))
+  dates_m_d_y <- as.character(format(as.Date(tmp[seq(1, 2*nrow(dat), 2)], format="%d/%m/%y"), "%m/%d/%y"))
+  times <- tmp[seq(2, 2*nrow(dat), 2)]
+  dat$Issue.Date <- paste(dates_m_d_y, times)
+  
+  return(dat)
+}
+
 dat2 <- process_names_promed_dat(dat2)
+dat2 <- process_issue_dates_promed_dat(dat2)
 
 ####################################
 ### test get_incid_DS1 function ###
 ####################################
 
 incid2 <- get_incid_DS1(dat2, 
-                       spec, 
-                       disease, 
-                       case_type, 
-                       location, 
-                       merge_rule = "median")
+                        spec, 
+                        disease, 
+                        case_type, 
+                        location, 
+                        merge_rule = "median")
 
 
 
 ### visualise incidence
-par(mfrow=c(1,1))
-plot(incid2$dates, incid2$incid, type="l", xlab="Time", ylab="Incidence")
+plot(incid2$dates, incid2$cum_incid, type="l", xlab="Time", ylab="Cumulative Incidence", xlim= c(as.Date("2014-01-01"), as.Date("2016-12-12")))
+plot(incid2$dates, incid2$incid, type="l", xlab="Time", ylab="Incidence", xlim= c(as.Date("2014-01-01"), as.Date("2016-12-12")))
+
+dat2 <- mRIIDSprocessData:::check_dat(dat2, 
+                                      spec, 
+                                      disease, 
+                                      case_type, 
+                                      location)
+points(dat2$Date, dat2$New.SC + dat2$New.SD, col="red")
 ### To do: add our estimate of daily incidence

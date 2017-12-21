62c77608e4a2917df311be6421859148b3ebf6eb
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2017-12-21 18:02:54 +0000

    reorganising the whole thing, using here pkg etc

--- R/fit_gravity_model.R
+++
@@ -1,5 +0,0 @@
-library(magrittr)
-library(geosphere)
-africa.adm0 <- "data/Geography/GravityModel/raw/adm0_centroids.tsv" %>%
-               read.csv(stringsAsFactors = FALSE, sep = "\t", header = FALSE)
-names(africa.adm0) <- c("country", "id", "lon", "lat", "pop")

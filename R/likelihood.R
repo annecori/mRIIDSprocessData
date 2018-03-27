47b98371e12450ef9f5238fdc97da8af83ec4fa9
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2018-03-27 17:14:14 +0100

    Style changes. Done for now.

--- R/likelihood.R
+++ R/likelihood.R
@@ -1,6 +1,6 @@
 ##' One step ahead likelihood
 ##'
-##' The parameters are the same as in lambda.j.t.
+##' The parameters are the same as in lambda_j_t.
 ##' @title
 ##' @param incid
 ##' @param window how many previous days are to be considered in determining lambda
@@ -18,7 +18,7 @@ log_likelihood_1step <- function(incid, window = 2, start, end, pij, R, si){
     for(i in start:end){
         i_t    <- incid[(i - window + 1):i, ]
         w_t    <- utils::tail(ws, nrow(i_t))
-        lambda <- lambda.j.t(pij, R, i_t, w_t)
+        lambda <- lambda_j_t(pij, R, i_t, w_t)
         loglh  %<>% c(sum(dpois(incid[i, ], lambda = lambda, log = TRUE)))
     }
     loglh

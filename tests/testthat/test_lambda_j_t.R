9e78f97b3c2f9cda224dfe1b16ef411850717d16
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2018-04-11 13:08:54 +0100

    expectetd dir structure for testthat

---
+++ tests/testthat/test_lambda_j_t.R
@@ -0,0 +1,13 @@
+## Only a test. The numbers do not make sense epidemiologically.
+testthat::test_that("mean for projection model is correct", {
+    p.movement <- matrix(c(0.5, 0.2, 0.3,
+                           0.2, 0.5, 0.2,
+                           0.3, 0.3, 0.5), nrow = 3, byrow = FALSE)
+
+    incidence  <- matrix(1:12, nrow = 4, byrow = FALSE)
+    r.t        <- c(7, 8, 4)
+    ws         <- 4:1
+    mu.correct <- c(286, 348, 386)
+    mu.test    <- lambda.j.t(p.movement, r.t, incidence, ws)
+    expect_true(all(mu.correct == mu.test))
+})
